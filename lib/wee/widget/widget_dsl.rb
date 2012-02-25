30d32ed32ed0934b7ecaf2ecdcb8138d7eee8c74
Arnold Noronha <arnstein87@gmail.com>
2012-02-25 14:15:57 -0800

    introducing, the super awesome, WidgetDsl.

---
+++ lib/wee/widget/widget_dsl.rb
@@ -0,0 +1,51 @@
+module Wee
+  module Widget
+    module WidgetDsl
+
+      def camelize(s)
+        s.split("_").collect { |i| i.capitalize }.join
+      end
+
+      def widgets
+        @widget_dsl_widgets ||= []
+      end
+
+      def name_hash
+        @widget_dsl_name_hash ||= {}
+      end
+
+      # name is a programmatic name, and will not show up in the external HTML
+      def w(type, params = {})
+        klass = eval(camelize(type.to_s))
+        widget = klass.new
+
+        if (params[:name])
+          name = params[:name]
+        end
+
+        if params[:text]
+          widget.text = params[:text]
+        end
+
+        if (name)
+          name_hash[name] = widget
+        end
+        puts "returning #{widget.inspect}"
+        widget
+      end
+
+      def findByName(name)
+        res = name_hash[name]
+        if (res.nil?)
+          raise "no widget found with name #{name}"
+        end
+        res
+      end
+
+      # alias for findByName
+      def f(name)
+        findByName(name)
+      end
+    end
+  end
+end
